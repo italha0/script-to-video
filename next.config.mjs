64d487b5208184395d6261441fbba7b7350a3345
italha77 <tq74198@gmail.com>
2025-09-09 16:34:57 +0530

    fsuh

--- next.config.mjs
+++ next.config.mjs
@@ -10,8 +10,20 @@ const nextConfig = {
       './remotion/**/*'
     ],
   },
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.module.rules.push({ test: /\.d\.ts$/, use: 'ignore-loader' });
+    if (isServer) {
+      const { IgnorePlugin } = require('webpack');
+      // Remotion renderer attempts to conditionally require many platform-specific
+      // compositor binaries. Only one will actually be needed at runtime, but
+      // webpack tries to resolve all static require() calls and fails for the
+      // ones not installed (arm64 variants). We ignore the unsupported ones.
+      config.plugins.push(
+        new IgnorePlugin({
+          resourceRegExp: /^@remotion\/compositor-(linux-arm64-(musl|gnu)|darwin-arm64|win32-x64|linux-arm64)$/
+        })
+      );
+    }
     return config;
   },
 };
