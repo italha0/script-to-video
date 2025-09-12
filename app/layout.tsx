4ba2f01b08880b860288bb9426174c0ec38b5f6a
italha77 <tq74198@gmail.com>
2025-09-12 11:04:30 +0530

    f

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 import { Suspense } from "react"
 import { Toaster } from "@/components/ui/toaster"
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 
 export const metadata: Metadata = {
